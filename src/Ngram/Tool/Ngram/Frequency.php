3b71c9fad0e6fd8590053aef06b8ca278ed71f7e
Ivo Nascimento <iannsp@gmail.com>
2015-01-30 17:10:22 -0200

    move frequency to work with array values as string or int

--- src/Ngram/Tool/Ngram/Frequency.php
+++ src/Ngram/Tool/Ngram/Frequency.php
@@ -5,15 +5,6 @@ class Frequency implements Tool
 {
     public static function get(array $ngram, array $ngramTo=null)
     {
-        $strings = [];
-        foreach ($ngram as $idx => $gram){
-            $strings[$idx] = implode(" ",$gram);
-        }
-        $result =  array_count_values($strings);
-        $count = [];
-        foreach($result as $nGramString=> $k){
-            $count[$nGramString]=$k;
-        }
-        return $count;
+        return array_count_values($ngram);
     }
 }
--- tests/src/nGram/Tool/FrequencyTest.php
+++ tests/src/nGram/Tool/FrequencyTest.php
@@ -3,15 +3,15 @@ namespace  Ngram\Tool\Ngram;
 use NGram\Frequency\Word;
 class FrequencyTest extends \PHPUnit_Framework_TestCase
 {
-    public function testCounter()
+    public function testCounterUnigram()
     {
-        $data = [['Ivo'],['viu'],['Ivo']];
+        $data = ['Ivo','viu','Ivo'];
         $expected =['Ivo'=>2, 'viu'=>1];
         $this->assertEquals($expected, Frequency::get($data));
     }
     public function testCounterBigram()
     {
-        $data = [['Ivo viu'],['viu Uva'],['Uva frase']];
+        $data = ['Ivo viu','viu Uva','Uva frase'];
         $expected =['Ivo viu'=>1, 'viu Uva'=>1,'Uva frase'=>1];
         $this->assertEquals($expected, Frequency::get($data));
     }
